9a972bfed2e6a79364948a914151c47125594de6
habibshrabon <snhabib777@gmail.com>
2021-11-02 23:27:37 +0600

    explore material ui utility

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import Service from "../Service/Service";
 import fluoride from "../../../images/fluoride.png";
 import cavity from "../../../images/cavity.png";
@@ -33,6 +33,16 @@ const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+        <Typography
+          sx={{ fontWeight: 500, color: "success.main", m: 5 }}
+          variant="h6"
+          component="div"
+        >
+          OUR SERVICES
+        </Typography>
+        <Typography sx={{ fontWeight: 600, m: 2 }} variant="h4" component="div">
+          Services We Provide
+        </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
