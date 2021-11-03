f8645421f16cec30c4431765ef9ab6874600c14a
habibshrabon <snhabib777@gmail.com>
2021-11-04 00:01:20 +0600

    Style booking card using material ui paper

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,11 +1,29 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import React from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 const Booking = ({ booking }) => {
   const { name, time, space } = booking;
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <h2>This is booking</h2>
+      <Paper elevation={3} sx={{ py: 5 }}>
+        <Typography
+          sx={{ color: "info.main", fontWeight: 600 }}
+          variant="h5"
+          gutterBottom
+          component="div"
+        >
+          {name}
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+          {time}
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+          {space} SPACES AVAILABLE
+        </Typography>
+        <Button variant="contained">BOOK APPOINTMENT</Button>
+      </Paper>
     </Grid>
   );
 };
