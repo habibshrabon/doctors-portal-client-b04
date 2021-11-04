7989937056bc450f6c33b9e367e5740aea652e89
habibshrabon <snhabib777@gmail.com>
2021-11-05 00:17:07 +0600

    Create firebase project and set environment variable

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: process.abort.env.REACT_APP_API_KEY,
+  authDomain: process.abort.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.abort.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.abort.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.abort.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.abort.env.REACT_APP_APP_ID,
+};
+export default firebaseConfig;
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBcXKjP0w9eMJMYZN16IpY7ZcT8nWQgFTk",
+//   authDomain: "doctors-portal-94850.firebaseapp.com",
+//   projectId: "doctors-portal-94850",
+//   storageBucket: "doctors-portal-94850.appspot.com",
+//   messagingSenderId: "585218065757",
+//   appId: "1:585218065757:web:387ff5c09be803b35c94b4",
+// };
+// export default firebaseConfig;
