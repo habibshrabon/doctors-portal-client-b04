513d61095cac5bfa13640edc019097e79a415a19
habibshrabon <snhabib777@gmail.com>
2021-11-07 20:13:56 +0600

    Create make admin page and api

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,43 @@
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const MakeAdmin = () => {
+  const [email, setEmail] = useState("");
+
+  const handelOnBlur = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handelAdminSubmit = (e) => {
+    const user = { email };
+    fetch("http://localhost:5000/users/admin", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+
+    e.preventDefault();
+  };
   return (
     <div>
-      <h2>Make and Admin</h2>
+      <h2>Make an Admin</h2>
+      <form onSubmit={handelAdminSubmit}>
+        <TextField
+          label="Email"
+          type="email"
+          onBlur={handelOnBlur}
+          variant="standard"
+        />
+        <Button variant="contained" type="submit">
+          Make Admin
+        </Button>
+      </form>
     </div>
   );
 };
